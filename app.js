8d7165ad06274cfdba6d5b5ea535742d2f04f1ae
Julia Zapata <juliamzapata@gmail.com>
2018-11-08 15:47:20 -0200

    primeira versão

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-var simulateBtn = document.getElementById('btn');
-simulateBtn.addEventListener("click", function(){
-    
+let buttonSimulate = document.getElementById('buttonSimulate');
+buttonSimulate.addEventListener("click", function(){
     window.location = 'simulator.html'
 });
 
-let loanValue = document.getElementById("InputLoanValue");
-let portionQtd = document.getElementById("portionNumId");
-let portionValue = loanValue / portionQtd;
-let totalLoan = portionQtd * portionValue;
+
 
 
---
+++ simulator.js
@@ -0,0 +1,17 @@
+let loanInput = document.getElementById("loanValue");
+let portionInput = document.getElementById("portionQtd");
+let autoSimulate = document.getElementById('autoSimulate');
+let print = document.getElementById("print");
+let printTwo = document.getElementById("print2");
+    
+autoSimulate.addEventListener("click", function(event){
+    event.preventDefault();
+    let loanValue = loanInput.value;
+let portionQtd = portionInput.value;        
+
+    let portionValue = loanValue / portionQtd;
+    let totalLoan = portionQtd * portionValue;
+    print.innerHTML = portionValue;
+    printTwo.innerHTML = totalLoan;
+});
+
