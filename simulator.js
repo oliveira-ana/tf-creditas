983de1cf49d230d53e0a0f538fc7511778649ae9
Ana-Oliveira <ana_oli@outlook.com>
2018-11-08 17:54:16 -0200

    consolidando formulas

--- simulator.js
+++ simulator.js
@@ -21,6 +21,8 @@ autoSimulate.addEventListener("click", function(event){
     let totalLoan = parseInt(portionQtd * portionValue);
     let numberParc = Math.log10((valueParc2-(taxaMensal*loanValue))/valueParc2)/Math.log10(1 + taxaMensal);
     let transformNumber = Math.abs(numberParc).toFixed();
+    let loanTax = (loanValue*taxaMensal)+loanValue;
     console.log(transformNumber);
     print.innerHTML = transformNumber;
+    print2.innerHTML = loanTax;
 });
