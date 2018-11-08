a9897f8db3fe22ee97b8f40b510b6b3c2fa61578
Julia Zapata <juliamzapata@gmail.com>
2018-11-08 14:27:13 -0200

    primeira versão do app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+let loanValue = document.getElementById("InputLoanValue");
+let portionQtd = document.getElementById("portionNumId");
+let portionValue = loanValue / portionQtd;
+let totalLoan = portionQtd * portionValue;
+
+
